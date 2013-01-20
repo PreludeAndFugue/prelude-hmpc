1313df335e84bd5a7d467ab9a0f7c5ca069f9e70
Gary Kerr <gdrummondk@gmail.com>
2013-01-20 17:21:59 +0000

    Homepage random image carousel
    Fixed error which meant that pictures in competitions which aren't
    completed could be viewed. This would reveal the photo's user.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 from random import shuffle
 import webapp2
 
@@ -25,13 +26,27 @@ class Home(BaseHandler):
         photo_keys = list(Photo.all(keys_only=True))
         shuffle(photo_keys)
         photos = []
-        for i, key in enumerate(photo_keys[:number]):
+        i = 0
+        for key in photo_keys:
             photo = Photo.get(key)
+            if photo.total_score == 0:
+                # only view photos belonging to completed competition -
+                # assuming all photos in completed competitions have non-zero
+                # score
+                # TODO: better way to implement this
+                logging.info("random images: image can't be used")
+                continue
             title = photo.title
             if not title:
                 title = 'Untitled'
             user = photo.user.username
             photos.append((i, key.id(), photo.url(size=800), title, user))
+            i += 1
+            if len(photos) == number:
+                # Once we have the required number of photos, we can quit the
+                # loop
+                break
+        logging.info('random photos: %s', photos)
         return photos
 
     def competitions_in_progress(self):
